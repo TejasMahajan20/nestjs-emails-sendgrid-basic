e5f82bd81fd986585359cb21c48a0d413e4fe5fa
Tejas Mahajan <tejasmahajan416@gmail.com>
2024-12-30 23:39:44 +0530

    updated error handling

--- src/modules/mail/sendgrid-client.ts
+++ src/modules/mail/sendgrid-client.ts
@@ -4,7 +4,7 @@ import { MailDataRequired, default as SendGrid } from '@sendgrid/mail';
 @Injectable()
 export class SendGridClient {
   private logger = new Logger(SendGridClient.name);
-  
+
   constructor() {
     //Get the API key from config service or environment variable
     SendGrid.setApiKey(process.env.SENDGRID_API_KEY);
@@ -15,8 +15,9 @@ export class SendGridClient {
       await SendGrid.send(mail);
       this.logger.log(`Email successfully dispatched to: ${mail.to as string}`);
     } catch (error) {
-      this.logger.error('Error while sending email', error);
-      throw error;
+      this.logger.error(`Error while sending email to: ${mail.to as string}`);
+      console.log(error);
+      // throw error;
     }
   }
 }
